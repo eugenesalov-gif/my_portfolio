133b16666627158269c19d17d3d8b1e29ee06346
eugenesalov-gif <eugenesalov@gmail.com>
2026-05-03 18:16:25 +0200

    Hotjar integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,6 +16,16 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased">
         {children}
+        <Script id="hotjar-tracking" strategy="afterInteractive">
+          {`(function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:6519150,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+        </Script>
       </body>
     </html>
   );
